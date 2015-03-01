04119637dbfade18ed16ccaf0b925c39f75d9fc1
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-03-01 22:16:48 +0100

    Better build

--- dist/MIDIPlayer.js
+++ dist/MIDIPlayer.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.MIDIPlayer=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.MIDIPlayer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // MIDIEvents : Read and edit events from various sources (ArrayBuffer, Stream)
 function MIDIEvents() {
 	throw new Error('MIDIEvents function not intended to be run.');
@@ -215,11 +215,14 @@ MIDIEvents.createParser=function(stream, startAt, strictMode) {
 							}
 							return event;
 							break;
-						 // Not implemented
 						case MIDIEvents.EVENT_META_TIME_SIGNATURE:
 							if(strictMode&&4!==event.length)
 								throw new Error(stream.pos()+' Bad metaevent length.');
 							event.data=stream.readBytes(event.length);
+							event.param1=event.data[0];
+							event.param2=event.data[1];
+							event.param3=event.data[2];
+							event.param4=event.data[3];
 							return event;
 							break;
 						case MIDIEvents.EVENT_META_SEQUENCER_SPECIFIC:
@@ -519,6 +522,7 @@ function MIDIPlayer(options) {
 		this.notesOn[i] = [];
 	}
 	this.midiFile = null;
+	window.addEventListener('unload', this.stop.bind(this));
 }
 
 // Parsing all tracks and add their events in a single event queue
